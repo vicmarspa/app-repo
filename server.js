026923c3dea24fce6024b02f27c8a476a7d90722
enzocalderonn <83834531+enzocalderonn@users.noreply.github.com>
2021-08-22 21:38:03 -0400

    v2 22/08

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static('./dist/frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: './dist/frontend/'}),
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080); 
+app.listen(process.env.PORT || 8080);
